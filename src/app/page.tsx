60bc3339059ea4811f2f7f5b4b58e18c13aa8294
eve seni <senieve@gmail.com>
2024-07-18 12:42:02 +0200

    assests and main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,38 @@
+import PatientForm from "@/components/forms/PatientForm";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-red-400"> Home</h1>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="mx-auto flex size-full flex-col py-10 max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+          <PatientForm />
+          <div className="text-14-regular flex justify-between mt-20">
+            <p className="text-dark-600 justify-items-end xl:text-left">
+              © 2024 CarePulse
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="hidden h-full object-cover md:block max-w-[50%]"
+      />
     </div>
   );
 }
