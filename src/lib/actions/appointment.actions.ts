b4e08862c7cdb47453c9643b65dc288e967faf81
eve seni <senieve@gmail.com>
2024-07-23 13:22:05 +0200

    admin page

--- src/lib/actions/appointment.actions.ts
+++ src/lib/actions/appointment.actions.ts
@@ -6,7 +6,8 @@ import {
   databases,
   PATIENT_COLLECTION_ID,
 } from "../appwrite.config";
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
+import { Appointment } from "../../../types/appwrite.types";
 
 export const CreateAppointment = async (
   appoinment: CreateAppointmentParams
@@ -32,6 +33,49 @@ export const getAppointment = async (appoinmentId: string) => {
       appoinmentId
     );
     return parseStringify(appointment);
+  } catch (error) {
+    console.error();
+  }
+};
+
+export const getRecentAppointmentList = async () => {
+  try {
+    const appointments = await databases.listDocuments(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      [Query.orderDesc("$createdAt")]
+    );
+    const initialCounts = {
+      scheduledCount: 0,
+      pendingCount: 0,
+      cancelledCount: 0,
+    };
+
+    const counts = (appointments.documents as Appointment[]).reduce(
+      (acc, appointment) => {
+        switch (appointment.status) {
+          case "scheduled":
+            acc.scheduledCount++;
+            break;
+          case "pending":
+            acc.pendingCount++;
+            break;
+          case "cancelled":
+            acc.cancelledCount++;
+            break;
+        }
+        return acc;
+      },
+      initialCounts
+    );
+
+    const data = {
+      totalCount: appointments.total,
+      ...counts,
+      documents: appointments.documents,
+    };
+
+    return parseStringify(data);
   } catch (error) {
     console.error();
   }
